083bcbf96e916bc1c5ef6e005e88942323206873
thanigaiguru <thanigaiguru786@gmail.com>
2023-09-02 14:12:59 +0530

    compose section component related logics added

--- src/components/ComposeSection.vue
+++ src/components/ComposeSection.vue
@@ -1,23 +1,69 @@
 <template>
-    
+    <div class="compose-section">
+      <textarea
+        v-model="messageText"
+        @input="adjustTextareaHeight"
+        @keyup.enter="submitMessage"
+        ref="messageInput"
+        placeholder="Enter your message."
+      ></textarea>
+      <button @click="submitMessage">Send</button>
+    </div>
 </template>
-
+  
 <script>
 export default {
     name : 'ComposeSection',
-    components: {
-
+    props: {
+        value: {
+            tyep: String,
+            default: ''
+        }
     },
     data(){
         return {
-            
+            messageText: this.value || ''
         }
-    },
-    computed: {
-
     },
     methods: {
         
+        // To increase the text Area height when we typing
+        adjustTextareaHeight() {
+            const textarea = this.$refs.messageInput;
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight + 'px';
+        },
+
+        // Handler to emit submit event
+        submitMessage() {
+            if (this.messageText.trim() === '') return;
+            this.$emit('submit', this.messageText);
+            this.messageText = '';
+            this.adjustTextareaHeight();
+        }
     }
-}
-</script>
+  };
+  </script>
+  
+  <style scoped>
+    .compose-section {
+        display: flex;
+        align-items: center;
+        margin-top: 12px;
+    }
+    
+    textarea {
+        flex-grow: 1;
+        padding: 8px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        resize: vertical;
+        overflow: hidden;
+    }
+    
+    button {
+        width: 60px;
+        margin-left: 12px;
+    }
+  </style>
+  
--- src/pages/chat/Chat.vue
+++ src/pages/chat/Chat.vue
@@ -1,23 +1,40 @@
 <template>
-    <div>Chat Section</div>
+    <div>
+        <div>Chat Section</div>
+        <compose-section v-model="message" @submit="sendReply" />
+    </div>
 </template>
 
 <script>
-    export default {
-        name : 'Chat',
-        components: {
 
-        },
-        data(){
-            return {
-                
-            }
-        },
-        computed: {
+import ComposeSection from "../../components/ComposeSection.vue";
 
-        },
-        methods: {
+export default {
+    name : 'Chat',
+    components: {
+        ComposeSection
+    },
+    data(){
+        return {
+            message: "Default msg for testing purpose",
+            messageList: "",
+           
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        sendReply(message) {
+            // Logic for adding msg to bottom of the compose section
+
+
+            // Scroll the conversation area to the bottom, if not already there
             
+
+            // Compose section shoulde be cleared
+            this.message = "";
         }
     }
+}
 </script>
